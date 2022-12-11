14b76d47320945076dff5841963b8049ba2140cd
Shobhandeb <devshobhan4@gmail.com>
2022-12-11 19:04:38 +0530

    functions_params.py updated

--- functions_params.py
+++ functions_params.py
@@ -23,3 +23,44 @@ def person(name,age,country='USA'):
     print(country)
 
 person('Soumyadeep',19,'India') # Here we overwrite the country with the default args of USA with India
+# Default argument never comes before the Non-Default arguments/Positional Args
+# Default params should be pushed to last
+
+# 3. *locations (Suppose you live in India, but to mention states * is used, like inplace of the default args)
+    #  * location is a vargs here or variable args
+def person(name,age,country='IN',*location):
+    print(name)
+    print(age)
+    print(country)
+    print(location)
+
+person('Shaan',18,'Raj','WB','MH')
+
+# To get a more clear idea about this, check the function below:
+
+def getEmployee(*empName):
+    print(empName)
+
+getEmployee('Rakesh','Mukesh','Harry') # Here we got the 3 employee name at once without passing 3 positional args instead of caling function 3 times.
+                                       # This is the main function for the * or vargs (variable length args)
+                                       # Data is taken in the form of a Tuple (* indicates the tuple, basically we dont want to modify the data)
+
+# Also, 
+
+# def getEmployee(empName):
+#     print(empName)
+# getEmployee('Rakesh','Mukesh','Harry') # Throws error: TypeError: getEmployee() takes 1 positional argument but 3 were given
+
+# 4. kwargs (**) - Keyword args
+
+def employee(name,age,*location,**worklocation):
+    print(name)
+    print(age)
+    print(location)
+    print(worklocation)
+
+employee('Shaan',24,'TX','WD','NY',winter='Boston',summer='San Fransisco')
+
+# if noticed: the *vargs gives tuple as output whereas **kwargs gives dictionary as output.
+# Always keep in mind that the Positional/Non-Default Params should be written first, default at the last, vargs/kwargs in the middle (*vargs,default args,**kwargs)
+# Basically the Keywords are the variable only passed in the form of kwargs
